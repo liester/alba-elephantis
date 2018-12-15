479d3ada89f4d9e15864b6465b01d1db736ba5c6
Robert Dobyns <robertddobyns@gmail.com>
2018-12-15 17:58:20 -0600

    Added different json item lists

--- src/Board.js
+++ src/Board.js
@@ -2,8 +2,7 @@ import React, { Component } from 'react'
 import Tile from './Tile'
 import PropTypes from 'prop-types'
 import { withStyles } from '@material-ui/core/styles'
-import Test from './Test'
-import itemsJson from './items.json'
+import itemsJson from './fiftyItems.json'
 
 const styles = theme => ({
     board: {
@@ -22,7 +21,7 @@ class Board extends Component {
         super(props)
         this.state = {
             items: itemsJson,
-            validIndexList: [0, 1, 2, 3, 4]
+            validIndexList: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49]
         }
 
     }
